b1a6d1f8f0b0b5efd17800dc3997b2b88dac81a9
Rofiyanto <pradiktabagus@gmail.com>
2022-10-28 20:35:29 +0700

    feat: commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': ['eslint --fix', 'eslint'],
-  '**/*.ts?(x)': () => 'npm run check-types',
+  '**/*.ts?(x)': () => ['npm run check-types', 'npm run test'],
   '*.json': ['prettier --write'],
 };
