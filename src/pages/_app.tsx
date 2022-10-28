ff186134ab63f0d6b7ef16163aecb52548dec7f7
Rofiyanto <pradiktabagus@gmail.com>
2022-10-28 17:12:19 +0700

    feat: vercel analytic

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import '../styles/global.css';
 
+import { Analytics } from '@vercel/analytics/react';
 import type { AppProps } from 'next/app';
 import React from 'react';
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <>
+    <Component {...pageProps} />
+    <Analytics />
+  </>
 );
 
 export default MyApp;
