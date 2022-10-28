186f4243977b557d4aea47d0059e2ee2a302b6d5
Rofiyanto <pradiktabagus@gmail.com>
2022-10-28 17:43:01 +0700

    chore: upgrade nextjs 13

--- src/components/logo/logo.tsx
+++ src/components/logo/logo.tsx
@@ -1,23 +1,15 @@
-/* eslint-disable import/no-cycle */
-import dynamic from 'next/dynamic';
 import Image from 'next/image';
+import Link from 'next/link';
 
-import type { TCustomLink } from '../customLink/customLink';
-
-const Link = dynamic<TCustomLink>(() =>
-  import('@/components').then((component) => component.Link)
-);
 export default function Logo() {
   return (
-    <Link
-      href="/"
-      className="relative h-[32px] w-[145px] no-underline hover:border-b-0"
-    >
+    <Link href="/" className="relative no-underline hover:border-b-0">
       <Image
         src="https://ecs7.tokopedia.net/assets-tokopedia-lite/v2/zeus/production/e5b8438b.svg"
         priority
         alt="logo"
-        layout="fill"
+        width={145}
+        height={32}
       />
     </Link>
   );
